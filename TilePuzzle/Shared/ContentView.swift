46721ddb0cd82b11937559ba3c03cbd93b3eb7bc
Joshua Stephenson <josh@brightmediums.com>
2021-10-01 23:11:12 -0400

    Board restricts tile sliding to only tiles adjascent of slot

--- TilePuzzle/Shared/ContentView.swift
+++ TilePuzzle/Shared/ContentView.swift
@@ -7,29 +7,6 @@
 
 import SwiftUI
 
-//public extension View {
-//    func offset(x: CGFloat, y: CGFloat) -> some View {
-//        return modifier(_OffsetEffect(offset: CGSize(width: x, height: y)))
-//    }
-//
-//    func offset(_ offset: CGSize) -> some View {
-//        return modifier(_OffsetEffect(offset: offset))
-//    }
-//}
-//
-//struct _OffsetEffect: GeometryEffect {
-//    var offset: CGSize
-//
-//    var animatableData: CGSize.AnimatableData {
-//        get { CGSize.AnimatableData(offset.width, offset.height) }
-//        set { offset = CGSize(width: newValue.first, height: newValue.second) }
-//    }
-//
-//    public func effectValue(size: CGSize) -> ProjectionTransform {
-//        return ProjectionTransform(CGAffineTransform(translationX: offset.width, y: offset.height))
-//    }
-//}
-
 struct BoardConstants {
     static let tileSize = 50;
     static let boardSize = 3;
@@ -37,22 +14,55 @@ struct BoardConstants {
 
 class BoardModel: ObservableObject {
     public var tiles:[BoardTile]
+    
+    // Key is the tile face value (number), value is position (index)
+    private var tileLookup:[Int:Int]
+    public var slotIndex: Int = -1
     public var slot: CGSize
-    public var n:Int = BoardConstants.boardSize
+    public var dimension:Int = BoardConstants.boardSize
     
     public var size:CGFloat {
-        return CGFloat(n) * CGFloat(BoardConstants.tileSize)
+        return CGFloat(dimension) * CGFloat(BoardConstants.tileSize)
     }
     
     init() {
         let count:Int = Int(pow(Double(BoardConstants.boardSize),2)) - 1
         var t:[BoardTile] = []
+        var l:[Int:Int] = [:]
         for i in 1...count{
-            let tile = BoardTile(number: i, offset: Board.offsetForTile(n: n, tile: i))
+            let tile = BoardTile(number: i, offset: Board.offsetForTile(n: dimension, tile: i))
             t.append(tile)
+            l[i] = i
         }
-        self.slot = Board.offsetForTile(n: n, tile: count + 1)
+        self.slotIndex = count + 1
+        self.slot = Board.offsetForTile(n: dimension, tile: count + 1)
         self.tiles = t
+        self.tileLookup = l
+    }
+    
+    func move(tile: BoardTile) -> Int {
+        if let idx = tileLookup[tile.number] {
+            let old = slotIndex
+            tileLookup[tile.number] = slotIndex
+            slotIndex = idx
+            slot = Board.offsetForTile(n: dimension, tile: slotIndex)
+            return old
+        }
+        return -1
+    }
+    
+    func isSlideable(tile: BoardTile) -> Bool {
+        if let tileIndex = tileLookup[tile.number] {
+            // slot is in the same row with tile and adjascent
+            if ((tileIndex - 1) / dimension == (slotIndex - 1) / dimension) && (abs(tileIndex - slotIndex) == 1) {
+                return true
+            // slot is in the same column with tile and adjascent
+            } else if abs(tileIndex - slotIndex) == dimension {
+                return true
+            }
+//            print("foo: \(((tileIndex - 1) / dimension == (slotIndex - 1) / dimension)), modulo: \(tileIndex % dimension), slot modulo: \(slotIndex % dimension)")
+        }
+        return false;
     }
 }
 
@@ -72,20 +82,24 @@ struct BoardTileView: View {
     @State var origin: CGSize
     var body: some View {
         Button("\(tile.number)") {
-            let temp = tile.origin
-            tile.origin = board.boardModel.slot
-            self.origin = tile.origin
-            board.boardModel.slot = temp
+            if board.model.isSlideable(tile: tile) {
+//                let temp = tile.origin
+                tile.origin = board.model.slot
+                self.origin = tile.origin
+//                board.boardModel.slot = temp
+                let idx = board.model.move(tile: tile)
+//                tile.origin = Board.offsetForTile(n: board.boardModel.dimension, tile: idx)
+            }
         }.offset(origin)
         .animation(.spring())
     }
 }
 
 struct Board: View {
-    public var boardModel: BoardModel
+    public var model: BoardModel
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
-            ForEach(boardModel.tiles, id: \.self) { tile in
+            ForEach(model.tiles, id: \.self) { tile in
                 BoardTileView(board: self, tile: tile, origin: tile.origin)
             }
         }
@@ -97,13 +111,12 @@ struct Board: View {
         return CGSize(width: col * BoardConstants.tileSize, height: row * BoardConstants.tileSize)
     }
     
-    
 }
 
 struct ContentView: View {
     var boardModel = BoardModel()
     var body: some View {
-        Board(boardModel: boardModel).frame(width: boardModel.size, height: boardModel.size, alignment: .topLeading)
+        Board(model: boardModel).frame(width: boardModel.size, height: boardModel.size, alignment: .topLeading)
     }
 }
 
