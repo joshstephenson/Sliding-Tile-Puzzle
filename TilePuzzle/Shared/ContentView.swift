60c2c24b8070fb54c5769fc2ae05ca956bc249c0
Joshua Stephenson <josh@brightmediums.com>
2021-10-03 10:25:46 -0400

    Simplified data model by moving position calc into Tile model and removing tileLookup

--- TilePuzzle/Shared/ContentView.swift
+++ TilePuzzle/Shared/ContentView.swift
@@ -29,10 +29,10 @@ enum BoardError: Error {
 }
 
 class BoardModel: ObservableObject {
-    public var tiles:[BoardTile]
+    public var tiles:[BoardTile] = []
     
     // slot is the empty tile
-    public var slotIndex: Int = -1
+    public var slotPosition: Int = -1
     
     // dimension is the width/height of the puzzle board
     public var dimension:Int
@@ -45,14 +45,18 @@ class BoardModel: ObservableObject {
         return manhattan == 0
     }
     
-    // Key is the tile face value (number), value is position (index)
-    private var tileLookup:[Int:Int]
+    @Published var progress: Double = 1.0
     
     // A measure of how many tiles are out of place
     private var hamming: Int = -1
     
     // Sum of distances between tiles and goal
-    private var manhattan: Int = -1
+    private var manhattan: Int = -1 {
+        didSet {
+            let maxManhattan = Double(tiles.count)
+            self.progress = hamming == 0 ? 1.0 : 1 - (Double(manhattan) / maxManhattan)
+        }
+    }
     
     // initialize a board of size dimension in its solved state
     init(dimension: Int) {
@@ -62,13 +66,12 @@ class BoardModel: ObservableObject {
         var t:[BoardTile] = []
         var l:[Int:Int] = [:]
         for i in 1...count{
-            let tile = BoardTile(number: i)
+            let tile = BoardTile(board: self, number: i, position: i)
             t.append(tile)
             l[i] = i
         }
-        self.slotIndex = count + 1
+        self.slotPosition = count + 1
         self.tiles = t
-        self.tileLookup = l
         processTiles()
     }
     
@@ -95,102 +98,106 @@ class BoardModel: ObservableObject {
         self.size = CGFloat(dimension) * BoardConstants.tileSize + CGFloat(dimension - 1) * BoardConstants.spacing
         print("Dimension: \(self.dimension) -- Tiles: \(numbers)")
         var t:[BoardTile] = []
-        var l:[Int:Int] = [:]
         
         for i in 1...numbers.count{
             let number = numbers[i-1]
             if number == 0 {
-                self.slotIndex = i
+                self.slotPosition = i
             }else{
-                let tile = BoardTile(number: number)
+                let tile = BoardTile(board: self, number: number, position: i)
                 t.append(tile)
-                l[number] = i
             }
         }
         self.tiles = t
-        self.tileLookup = l
         processTiles()
-    }
-    
-    func indexForTile(tile: BoardTile) -> Int {
-        return tileLookup[tile.number]!
     }
     
     func move(tile: BoardTile, block: (CGSize, Bool) -> Void) {
         if isSlideable(tile: tile) {
-            if let idx = tileLookup[tile.number] {
-                let oldSlot = Board.offsetForTile(n: dimension, positionIndex: slotIndex)
-                
-                // tile's index becomes the slot's index
-                tileLookup[tile.number] = slotIndex
-                
-                // New slot index becomes the tile's index
-                slotIndex = idx
-                
-                processTiles()
-                
-                // Let the view update itself
-                block(oldSlot, isSolved)
-            }
+            let oldPosition = tile.position
+            let oldSlot = Board.offsetForPosition(n: dimension, position: slotPosition)
+            let oldManhattan = tile.manhattan
+            
+            // tile's position becomes the slot's
+            tile.position = slotPosition
+            
+            // Update manhattan so we know when the puzzle is solved
+            manhattan -= (oldManhattan - tile.manhattan)
+            
+            // New slot index becomes the tile's index
+            slotPosition = oldPosition
+
+            // Let the view update itself
+            block(oldSlot, isSolved)
         }
     }
     
     // We should only be able to slide tiles adjascent to empty slot
     private func isSlideable(tile: BoardTile) -> Bool {
-        if let tileIndex = tileLookup[tile.number] {
-            // slot is in the same row with tile and adjascent
-            if ((tileIndex - 1) / dimension == (slotIndex - 1) / dimension) && (abs(tileIndex - slotIndex) == 1) {
-                return true
-            // slot is in the same column with tile and adjascent
-            } else if abs(tileIndex - slotIndex) == dimension {
-                return true
-            }
+        // slot is in the same row with tile and adjascent
+        if ((tile.position - 1) / dimension == (slotPosition - 1) / dimension) && (abs(tile.position - slotPosition) == 1) {
+            return true
+        // slot is in the same column with tile and adjascent
+        } else if abs(tile.position - slotPosition) == dimension {
+            return true
         }
         return false;
     }
     
+    // Calculate our initial hamming and manhattan
     private func processTiles() {
         var hamming = 0
         var manhattan = 0
         for tile in tiles {
-            if let manh = try? manhattanForTile(tile: tile) {
-                if manh > 0 {
-                    hamming += 1
-                }
-                manhattan += manh
+            if tile.manhattan > 0 {
+                hamming += 1
+                manhattan += tile.manhattan
             }
         }
         self.hamming = hamming
         self.manhattan = manhattan
-        print("Hamming: \(hamming), Manhattan: \(manhattan)")
     }
     
-    private func manhattanForTile(tile: BoardTile) throws -> Int {
-        guard let index = tileLookup[tile.number] else {
-            throw BoardError.invalidTileIndex
-        }
-        let col = (index - 1) % dimension + 1
-        let row = (index - 1) / dimension + 1
-        let value = tile.number
-        var finalCol = value % dimension
-        var finalRow:Int
-        if (finalCol == 0) {
-            finalRow = value / dimension;
-            finalCol = dimension;
-        } else {
-            finalRow = (value / dimension) + 1;
-        }
-        let manhattan = abs(finalRow - row) + abs(finalCol - col)
-//        print("\(tile.number), row: \(row), col: \(col), finalRow: \(finalRow), finalCol: \(finalCol), manh: \(manhattan)")
-        return manhattan
-    }
 }
 
 class BoardTile: NSObject {
+    var board: BoardModel
     var number: Int
+    var col: Int = -1
+    var row: Int = -1
+    var manhattan: Int = -1
+    var position: Int {
+        didSet {
+            updateCalculatedAttributes()
+        }
+    }
     
-    init(number: Int) {
+    init(board: BoardModel, number: Int, position: Int) {
+        self.board = board
         self.number = number
+        self.position = position
+        super.init()
+        updateCalculatedAttributes()
+    }
+    
+    private func updateCalculatedAttributes() {
+        self.col = (position - 1) % board.dimension + 1
+        self.row = (position - 1) / board.dimension + 1
+        self.manhattan = calculateManhattan()
+    }
+    
+    private func calculateManhattan() -> Int {
+        var finalCol = number % board.dimension
+        var finalRow:Int
+        if (finalCol == 0) {
+            finalRow = number / board.dimension;
+            finalCol = board.dimension;
+        } else {
+            finalRow = (number / board.dimension) + 1;
+        }
+        let manhattan = abs(finalRow - row) + abs(finalCol - col)
+//        print("\(number), row: \(row), col: \(col), finalRow: \(finalRow), finalCol: \(finalCol), manh: \(manhattan)")
+        return manhattan
     }
 }
 
@@ -229,15 +236,15 @@ struct Board: View {
     var body: some View {
         ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
             ForEach(model.tiles, id: \.self) { tile in
-                BoardTileView(board: self, tile: tile, origin: Board.offsetForTile(n: model.dimension, positionIndex: model.indexForTile(tile: tile)))
+                BoardTileView(board: self, tile: tile, origin: Board.offsetForPosition(n: model.dimension, position: tile.position))
                     .background(Color.clear)
             }
         }
     }
     
-    static func offsetForTile(n: Int, positionIndex: Int) -> CGSize {
-        let col = CGFloat((positionIndex - 1) % n)
-        let row = CGFloat((positionIndex - 1) / n)
+    static func offsetForPosition(n: Int, position: Int) -> CGSize {
+        let col = CGFloat((position - 1) % n)
+        let row = CGFloat((position - 1) / n)
         return CGSize(width: col * BoardConstants.tileSize + (col - 1) * BoardConstants.spacing,
                       height: row * BoardConstants.tileSize + (row - 1) * BoardConstants.spacing)
     }
@@ -247,9 +254,20 @@ struct Board: View {
 struct ContentView: View {
     var boardModel = try? BoardModel(filename: "3x3-01.txt")
     var body: some View {
-        Board(model: boardModel!).frame(width: boardModel!.size, height: boardModel!.size, alignment: .topLeading)
-            .background(Color("Board"))
+        VStack(alignment: .center, spacing: 5.0) {
+//            Rectangle()
+//                .frame(maxWidth: .infinity)
+//                .frame(height: 20.0)
+//                .background(Color.clear)
+            Board(model: boardModel!).frame(width: boardModel!.size, height: boardModel!.size, alignment: .topLeading)
+                .background(Color("Board"))
+        }
     }
+    
+//    private func progressColor(_ progress: Double) -> Color {
+//        print(progress)
+//        return Color(hue: 0.9, saturation: progress, brightness: 1.0)
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
