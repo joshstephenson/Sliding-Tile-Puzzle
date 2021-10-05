b157b4746b42144d4c725530f4d8c996fce61f15
Joshua Stephenson <josh@brightmediums.com>
2021-10-05 16:09:19 -0400

    Test comments

--- TilePuzzle/Tests macOS/SolverTests.swift
+++ TilePuzzle/Tests macOS/SolverTests.swift
@@ -92,11 +92,4 @@ class SolverTests: XCTestCase {
         XCTAssertEqual(solver.moves(), -1)
     }
 
-//    func testPerformanceExample() throws {
-//        // This is an example of a performance test case.
-//        self.measure {
-//            // Put the code you want to measure the time of here.
-//        }
-//    }
-
 }
